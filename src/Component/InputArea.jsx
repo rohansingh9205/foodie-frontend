b2e185a44757989e423d7a8099e5965fa6fc08a1
Rohan <rohansin48@gmail.com>
2026-05-10 01:42:05 +0530

    fix iphone date overflow

--- src/Component/InputArea.jsx
+++ src/Component/InputArea.jsx
@@ -1,22 +1,40 @@
 import React from 'react'
 
-const InputArea = ({label, name, value, onChange, type='text', placeholder}) => {
-    const today = new Date().toISOString().split("T")[0];
+const InputArea = ({
+  label,
+  name,
+  value,
+  onChange,
+  type = 'text',
+  placeholder
+}) => {
+
+  const today = new Date().toISOString().split("T")[0];
+
   return (
+
     <div className='mb-4'>
-        <label htmlFor={name} className='block text-black mb-2 font-semibold'>{label}</label>
-        <input
+
+      <label
+        htmlFor={name}
+        className='block text-black mb-2 font-semibold'
+      >
+        {label}
+      </label>
+
+      <input
         id={name}
         name={name}
         value={value}
         onChange={onChange}
         placeholder={placeholder}
-        type={type} 
+        type={type}
         min={type === "date" ? today : undefined}
-        className='w-full px-4 py-2 rounded border placeholder:text-xs'
-        />
+        className='w-full min-w-0 px-4 py-2 rounded border placeholder:text-xs'
+      />
 
     </div>
+
   )
 }
 
